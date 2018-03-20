1524cbc491b6d584698a4d09014d6c3897354b17
luftbuefel <luftbuefel@users.noreply.github.com>
2018-03-19 20:27:53 -0700

    Added a working version of the game
    This is a working version of the Flappy Bird Microbit game. It has scrolling walls and keeps score. In the future I would like to control the bird by tilting the Microbit and using accelerometer information rather than by pressing buttons. I may also like to add a replay the game function controlled by shaking the Microbit instead of requiring the reset button to be pressed. It might also be fun to add a sound track to the game; I am not sure if it would become annoying or interfere with the score sounds.

--- microbitFlappyBird.py
+++ microbitFlappyBird.py
@@ -1,44 +1,121 @@
 from microbit import *
+from random import *
+import music
+
+
+class Wall:
+    column = -1
+    hole = -1
+
 
 birdRow = 2
-birdColumn = 1
+BIRD_COLUMN = 1
 birdCanMove = True
-
+birdBrightness = 9
+score = 0
+WALL_MOVE_TIMER = 800
+WALL_BRIGHTNESS = 3
 EMPTY_ROW = "00000"
+wallMoveTimer = WALL_MOVE_TIMER
+walls = []
+wallsUpdated = True
+gamePlaying = True
+
 
 def moveUp():
     global birdRow
-    if birdRow<0:
-        birdRow-=1
+    if birdRow > 0:
+        birdRow -= 1
+    updateDisplay()
     return
+
 
 def moveDown():
     global birdRow
-    if birdRow<4:
-        birdRow+=1
+    if birdRow < 4:
+        birdRow += 1
+    updateDisplay()
     return
+
+
+def addAWall():
+    global walls
+    newWall = Wall()
+    newWall.column = 5
+    newWall.hole = randint(0, 4)
+    walls.append(newWall)
+    return
+
+
+#this function controls the data for positioning the walls but not displaying
+def moveWalls():
+    global score
+    for wall in walls:
+        wall.column -= 1
+        #when a wall is in the first column, it is time to add a new one
+        if(wall.column==BIRD_COLUMN and wall.hole!=birdRow):
+            gameOver()
+            return
+        if(wall.column == 0):
+            addAWall()          
+            #add a point to the score and play a score sound
+            score += 1         
+            music.play(music.BA_DING)
+        if(wall.column < 0):
+            #delete the wall from the list
+            del walls[0]
+    updateDisplay()
+    return
+
 
 def updateDisplay():
-    newDisplay = ""
-    nextRow=""
-    for row in range(0,4):
-        nextRow += EMPTY_ROW
-        if(birdRow==row):
-            nextRow = EMPTY_ROW[:birdColumn]+"9"+EMPTY_ROW[birdColumn:]
-        newDisplay+=nextRow
+    screenContents = ""
+    #move the bird up and down
+    for row in range(0, 5):
+        nextRow = EMPTY_ROW
+       
+        for wall in walls:
+          wallColumn = wall.column
+          hole = wall.hole
+          if(row!=hole):
+            nextRow = nextRow[:wallColumn]+str(WALL_BRIGHTNESS)+nextRow[wallColumn:]
         if(row != 4):
-            nextRow+=":"
-    display.show(Image(newDisplay))
+            nextRow += ":"
+        #draw the bird
+        if(birdRow == row):            
+            nextRow = nextRow[:BIRD_COLUMN]+str(birdBrightness)+nextRow[BIRD_COLUMN+1:]
+        #go through and add the walls
+        screenContents += nextRow
+    display.show(Image(screenContents))
     return
 
 
-while True:
-    if button_a.is_pressed():
+def gameOver():
+    global gamePlaying
+    gamePlaying = False
+    display.show(Image.SKULL)
+    sleep(1000)
+    display.scroll("Score: "+str(score), delay=100,loop=True)
+    return
+    
+    
+#add an initial wall
+addAWall();
+
+#Update each frame
+while gamePlaying:    
+    if button_a.is_pressed() and birdCanMove is True:
+        birdCanMove = False
         moveUp()
-    if button_b.is_pressed():
+    elif button_b.is_pressed() and birdCanMove is True:
+        birdCanMove = False
         moveDown()
-    #check if the buttons have been upressed
-    #if button_a.is_pressed()
-    updateDisplay()
-
-
+    if not button_a.is_pressed() and not button_b.is_pressed():
+        birdCanMove = True
+  
+    #decrement the wall timer
+    wallMoveTimer -= 1
+    #check if walls need to be moved
+    if(wallMoveTimer==0):
+        moveWalls()
+        wallMoveTimer = WALL_MOVE_TIMER
